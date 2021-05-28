1fde7516258205cf66d82182413149f2f107db36
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-28 19:58:05 -0300

    Primeiro commit

--- BlurSwiftUI/ContentView.swift
+++ BlurSwiftUI/ContentView.swift
@@ -8,9 +8,117 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var isPasswordVisible = false
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack(alignment: .bottom) {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Welcome!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .foregroundColor(.gray)
+                        .fontWeight(.semibold)
+                    
+                    TextField("Email", text: $email)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                
+                VStack(alignment: .leading) {
+                    Text("Password")
+                        .foregroundColor(.gray)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        if isPasswordVisible {
+                            TextField("Password", text: $password)
+                        } else {
+                            SecureField("Password", text: $password)
+                        }
+                        
+                        Button(action: {
+                            isPasswordVisible.toggle()
+                        }, label: {
+                            Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+                                .foregroundColor(.black)
+                        })
+                    }
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Text("Forgot password or login?")
+                            .foregroundColor(.white)
+                    })
+                }
+                
+                Button(action: {}, label: {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 70)
+                        .background(Color.black.opacity(0.7))
+                        .cornerRadius(8)
+                })
+                .padding(.top)
+                
+                Button(action: {}, label: {
+                    Text("I don't have an account")
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 70)
+                        .background(RoundedRectangle(cornerRadius: 8).strokeBorder(Color.gray, lineWidth: 1))
+                        .cornerRadius(8)
+                })
+                
+                Button(action: {}, label: {
+                    Text("Contact us")
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 70)
+                })
+            }
             .padding()
+            .background(BlurView(style: .systemUltraThinMaterialDark)) // temp
+            .cornerRadius(15)
+        }
+        .ignoresSafeArea(.all, edges: .bottom)
+    }
+}
+
+struct BlurView: UIViewRepresentable {
+    var style = UIBlurEffect.Style.dark
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,23 @@
+# Tela de login usando SwiftUI - BlurEffect
+Encontrei essa [tela de login](https://www.instagram.com/p/CFZnfcyiZ5b/) pelo Instagram e achei interessante implementar o efeito de blur. Nunca tinha feito e foi bom pra treinar.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/120048864-bbdee200-bfee-11eb-832f-d3feccefaf23.png" width="400">
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/CunVTYMlF1E)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)

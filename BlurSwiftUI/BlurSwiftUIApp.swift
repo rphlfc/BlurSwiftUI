8e650a94af4d45c1d530a8eb78a0f22e3be68cf6
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-28 18:59:25 -0300

    Initial Commit

---
+++ BlurSwiftUI/BlurSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurSwiftUIApp.swift
+//  BlurSwiftUI
+//
+//  Created by Raphael Cerqueira on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BlurSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
